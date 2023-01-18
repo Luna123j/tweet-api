2796c057e247ddcf8812af6d4f4fac06482c9d48
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-18 17:19:28 -0500

    add fundtion to add user

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,15 +1,31 @@
 const db = require('../../configs/db.config');
 
 const getAllUsers = () => {
-	return db.query("SELECT * FROM users;").then(data => {
-		return data.rows;
-	})
+  return db.query("SELECT * FROM users;").then(data => {
+    return data.rows;
+  })
 }
 
 const getUserById = id => {
-	return db.query("SELECT * FROM users; WHERE id = $1", [id]).then(data => {
-		return data.rows;
-	})
+  return db.query("SELECT * FROM users WHERE id = $1", [id]).then(data => {
+    return data.rows;
+  })
 }
 
-module.exports = {getAllUsers, getUserById}
+const createUser = (username, password) => {
+
+  //check if username exist
+  return db.query("SELECT count(*) FROM users WHERE username = $1", [username]).then(data => {
+    if (data.rows > 0) {
+      return "User exist"
+    }
+    
+    return db.query("INSERT INTO users (username, password) VALUES ($1,$2)", [username, password]).then(data => {
+      return "User added"
+    })
+  })
+
+}
+
+
+module.exports = { getAllUsers, getUserById, createUser}
