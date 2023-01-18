b32dda8f058940f8c048eeca3ca3be742555db65
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-18 14:43:34 -0500

    init commit

---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const users = require('../db/queries/users');
+
+
+router.get('/', (req, res) => {
+  users.getAllUsers().then(data => {
+    console.log(data);
+    res.json({users: data});
+  })
+});
+
+
+module.exports = router;
+
