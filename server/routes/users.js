873d7422de3b8282acba0e8b3f71619cdadb5264
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-18 23:30:58 -0500

    write test for loggin and fix errors, passed test

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,7 @@ const users = require('../db/queries/users');
 router.get('/', (req, res) => {
   users.getAllUsers().then(data => {
     console.log(data);
-    res.json({users: data});
+    res.status(200).json({users: data});
   })
 });
 
@@ -21,17 +21,21 @@ router.get('/:id', (req, res) => {
 
 router.post('/login',(req,res)=>{
   const user = req.body;
+  console.log(req.body);
   const hashedPassword = bcrypt.hashSync(user.password, 10);
-  users.getUserByusername(user.username).then(data => {
-    if(data === null){
-      res.json({message: "User dose not exist"})
+  users.getUserByusername(user.username)
+  .then(data => {
+    console.log(data);
+    if(data.length < 1){
+      return res.status(404).json({message: "User dose not exist"})
     }
 
-    if(!bcrypt.compareSync(data.password,hashedPassword)){
-      res.json({message: "Wrong Password"})
+    if(!bcrypt.compareSync(data[0].password,hashedPassword)){
+      return res.status(404).json({message: "Wrong Password"})
     }
 
-    req.session.userId = data.id;
+    req.session.userId = data[0].id;
+    return res.status(200).json({message: "logged in"});
   })
 })
 
@@ -40,14 +44,14 @@ router.post('/register', (req, res) => {
   users.createUser(user.username,bcrypt.hashSync(user.password, 10)).then(data => {
     console.log(data);
     req.session.userId = data.id;
-    res.json({message: data});
+    res.status(200).json({message: data});
   })
 });
 
 
 router.post("/logout", (req, res) => {
   req.session = null; //clear all cookies
-  res.json({message: "logged out"});
+  res.status(200).json({message: "logged out"});
 });
 
 module.exports = router;
--- server/test/user_test.js
+++ server/test/user_test.js
@@ -1,18 +1,58 @@
-const { assert } = require('chai');
+const { assert,expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const users = require('../db/queries/users.js');
+
+chai.use(chaiHttp);
 
 const testUser1 = {id:1, username:'rick.sandchez@gmail.com',password: 'picklerick'};
 const testUser2 ={username: 'test', password: 'test'};
 
-describe('createUser', function() {
-  it('should return user exist', function() {
-    const message = users.createUser(testUser1.username,testUser1.password)
-    assert(message, "Username exist");
-  });
 
-  it('should return user added successfully ', function() {
-    const message = users.createUser(testUser2.username,testUser2.password)
-    assert(message, "User added");
-  });
+describe('user routes',()=>{
+  describe('test POST route /users/login',()=>{
+    it('should return login message if user exist', function() {
+      return chai.request('http://localhost:8080')
+      .post("/users/login")
+      .type('form')
+      .send({ username: 'rick.sandchez@gmail.com', password: 'picklerick' })
+      .then((res)=>{
+        expect(res).to.have.status(200);
+        assert(res.body.message, "logged in");
+      })
+      .catch(function (err) {
+        throw err;
+     })
+    });
   
-});
+    it('should return error message if user does not exist', function() {
+      return chai.request('http://localhost:8080')
+      .post("/users/login")
+      .type('form')
+      .send({ username: 'test', password: 'test' })
+      .then((res)=>{
+        expect(res).to.have.status(404);
+        assert(res.body.message, "User dose not exist")
+      })
+      .catch(function (err) {
+        throw err;
+     })
+      
+    });
+
+    it('should return error message if user enter wrong password', function() {
+      return chai.request('http://localhost:8080')
+      .post("/users/login")
+      .type('form')
+      .send({ username: 'rick.sandchez@gmail.com', password: 'test' })
+      .then((res)=>{
+        expect(res).to.have.status(404);
+        assert(res.body.message, "Wrong Password")
+      })
+      .catch(function (err) {
+        throw err;
+     })
+      
+    });
+  })
+})
