5627ff34d4bbdbfc77a8de501fcc79250e5de1df
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-20 12:23:42 -0500

    add notice for seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,5 +1,6 @@
 -- seeds/01_users.sql
 -- users seeds
+-- notice: don't use this data for login, because the password is not in bycrypt
 INSERT INTO users (username, password) VALUES ('rick.sandchez@gmail.com', 'picklerick');
 INSERT INTO users (username, password) VALUES ('lisa.simpson@gmail.com', 'ehhhhh');
 INSERT INTO users (username, password) VALUES ('link@yahoo.com', 'hyrule');
